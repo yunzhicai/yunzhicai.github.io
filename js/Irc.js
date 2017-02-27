bf9a1445eeeda8de8c194086064e970e90becc58
jeryqwq <you@example.com>
2017-02-27 21:49:26 +0800

    “上传网站源码到github”

---
+++ js/Irc.js
@@ -0,0 +1,141 @@
+﻿
+function begin() {
+
+    var __lis = [{ "offset": 1198, "text": "We don't talk anymore " },
+                { "offset": 4100, "text": "We don't talk anymore " },
+                { "offset": 6350, "text": "We don't talk anymore Like we used to do" },
+                { "offset": 11100, "text": "We don't laugh anymore" },
+                { "offset": 13600, "text": "What was all of it for ? " },
+                { "offset": 16100, "text": "We don't talk anymore " },
+                { "offset": 18100, "text": "Like we used to do" },
+                { "offset": 21100, "text": "I just heard you found the one you'vebeen looking" },
+                { "offset": 24100, "text": "You been looking for" },
+                { "offset": 27100, "text": "I wish i would've konwn that wasn't me " },
+                { "offset": 31000, "text": "Cause even after all this time i still wonder" },
+                { "offset": 35000, "text": "Why i can't move on ?" },
+                { "offset": 37000, "text": "Just the way you dance so easliy " },
+                { "offset": 42000, "text": "Don't wanna know " },
+                { "offset": 43200, "text": "The kinda dress you're wearin' tonight " },
+                { "offset": 45300, "text": "If he's holdin' onto you so tight " },
+                { "offset": 48000, "text": "The way i did before" },
+                { "offset": 51000, "text": "I overdosed" },
+                { "offset": 52000, "text": "Should've known your love was game " },
+                { "offset": 55000, "text": "Now I can't get'cha outof my brain" },
+                { "offset": 58000, "text": "Ooh it's such a shame " },
+                { "offset": 61000, "text": "We don't talk anymore" },
+                { "offset": 63000, "text": "We don't talk anymore" },
+                { "offset": 66000, "text": "We don't talk anymore" },
+                { "offset": 67500, "text": "Like we used to do" },
+                { "offset": 71000, "text": "We don't laugh anymore" },
+                { "offset": 74000, "text": "What was all of it for ? " },
+                { "offset": 76000, "text": "We don't talk anymore " },
+                { "offset": 78000, "text": "Like we used to do" },
+                { "offset": 81000, "text": "Selena : I just hope you'r lyin' next to somebody " },
+                { "offset": 84000, "text": "Know it's hard to love yalike me" },
+                { "offset": 87000, "text": "Must be a good reason that you're gone " },
+                { "offset": 92000, "text": "Every now and then I think you might want me to come show up your door" },
+                { "offset": 98000, "text": "But I'm just too afraidthat i'll be worng " },
+                { "offset": 103000, "text": "Don't wanna know " },
+                { "offset": 102500, "text": "If you'ra lookin' intoher eyes" },
+                { "offset": 104000, "text": "If she'sholdin' onto you so tight  " },
+                { "offset": 108000, "text": "The way i did before  " },
+                { "offset": 109000, "text": "I overdosed " },
+  { "offset": 111000, "text": "Should I know your lovewas a game" },
+   { "offset": 113000, "text": "Now I can't get'cha outof my brain " },
+   { "offset": 115000, "text": "Ooh it's such a shame" },
+    { "offset": 118000, "text": "We don't talk anymore" },
+     { "offset": 121000, "text": "We don't talk anymore" },
+      { "offset": 124000, "text": "We don't talk anymore" },
+       { "offset": 126000, "text": "Like we used to do" },
+        { "offset": 128000, "text": "We don't talk anymore" },
+    { "offset": 130000, "text": "We don't laugh anymore " },
+{ "offset": 131000, "text": "What was all of it for ?" },
+{ "offset": 133000, "text": "We don't talk anymore" },
+{ "offset": 135000, "text": "Like we used to do" },
+{ "offset": 146000, "text": "Charlie: Like we used to do" },
+{ "offset": 157000, "text": "Don't wanna know The kinda dress you're wearin' tonight " },
+{ "offset": 161000, "text": "If he's givin' it to you just right " },
+{ "offset": 165000, "text": "The way i did before  " },
+{ "offset": 1665000, "text": "I overdosed " },
+{ "offset": 168000, "text": "Should I know your lovewas a game" },
+{ "offset": 171000, "text": "Now I can't get'cha outof my brain " },
+{ "offset": 173500, "text": "Ooh it's such a shame" },
+{ "offset": 180000, "text": "We don't talk anymore" },
+{ "offset": 182000, "text": "We don't talk anymore Like we used to do " },
+{ "offset": 186000, "text": "We don't talk anymore" },
+{ "offset": 188500, "text": "What was all of it for ?" },
+{ "offset": 190000, "text": "We don't talk anymore Like we used to do " },
+{ "offset": 195000, "text": "We don't talk anymore" },
+{ "offset": 202000, "text": "The way i did before " },
+{ "offset": 208000, "text": "We don't talk anymore" },
+{ "offset": 210000, "text": "Ooh Woo " },
+{ "offset": 214000, "text": "S: Ooh it's such a shame " },
+{ "offset": 216000, "text": "We don't talk anymore" }];
+var __eul = document.getElementById("lrc");
+(function () {
+    for (var i = 0; i < __lis.length; i++) {
+        var eli = document.createElement("li");
+        eli.innerText = __lis[i].text;
+        __eul.appendChild(eli);
+    }
+})();
+
+var __freq = 20;
+var __fraction = 2 / 5;
+
+var __go = function (_lineno) {
+    var _time;
+    if (_lineno === 0) {
+        _time = __lis[_lineno].offset;
+    } else {
+        _time = __lis[_lineno].offset - __lis[_lineno - 1].offset;
+    }
+    var _timer = setTimeout(__go.bind(this, _lineno + 1), _time);
+
+    // 高亮下一行歌词
+    if (_lineno > 0) {
+        __eul.children[_lineno - 1].setAttribute("class", "");
+    }
+    var _ep = __eul.children[_lineno];
+    _ep.setAttribute("class", "z-crt");
+
+    // 满足需求5，6
+    var _scrollTop;
+    if (_ep.offsetTop < __eul.clientHeight * __fraction) {
+        _scrollTop = 0;
+    } else if (_ep.offsetTop > (__eul.scrollHeight - __eul.clientHeight * (1 - __fraction))) {
+        _scrollTop = __eul.scrollHeight - __eul.clientHeight;
+    } else {
+        _scrollTop = _ep.offsetTop - __eul.clientHeight * __fraction;
+    }
+
+    // 如用户拖动滚动条导致当前显示行超出显示区域范围，下一行直接定位到当前显示行
+  
+        __eul.scrollTop = _scrollTop;
+
+};
+/**
+ * 歌词单行滚动实现
+ */
+__scroll = function (_crt, _dst, _step) {
+    if (Math.abs(_crt - _dst) < _step) {
+        return;
+    }
+    if (_crt < _dst) {
+        __eul.scrollTop += _step;
+        _crt += _step;
+    } else {
+        __eul.scrollTop -= _step;
+        _crt -= _step;
+    }
+    setTimeout(__scroll.bind(this, _crt, _dst, _step), __freq);
+};
+
+
+
+__go(0)
+
+
+
+
+}
---
+++ js/SetVideo.js
@@ -0,0 +1,250 @@
+﻿
+var audio = new Audio("Charlie Puth feat. Selena Gomez - We Don't Talk Anymore.mp3");
+var audio1 = new Audio("gnash,Olivia O'Brien - i hate u i love u.mp3");
+    var type = true;
+    function autoPlay() {
+    audio.loop = "loop";
+    audio.currentTime = 0;
+    if (type == true) {
+        audio.play();
+        type = false;
+}
+    else {
+        audio.pause();
+        type = true;
+    }
+}
+
+function autoPlay1() {
+    audio1.loop = "loop";
+    audio1.currentTime = 0;
+    if (type == true) {
+        audio1.play();
+        type = false;
+    }
+    else {
+        audio1.pause();
+        type = true;
+    }
+}
+function change() {
+    var lipan = Math.random();
+    lipan = lipan * 6;
+    lipan = Math.round(lipan);
+    var lipath = "";
+    switch (lipan) {
+        case 1: lipath = "img/3.JPG"; break;
+        case 2: lipath = "img/4.JPG"; break;
+        case 3: lipath = "img/5.JPG"; break;
+        case 4: lipath = "img/6.JPG"; break;
+        case 5: lipath = "img/7.JPG"; break;
+        case 6: lipath = "img/8.JPG"; break;
+    }
+    document.body.style.backgroundImage = "URL(" + lipath + ")";
+
+}
+
+setInterval("change()", 10000);
+(function Load() {
+
+    var tiltSettings = [
+    {},
+    {
+        movement: {
+            imgWrapper : {
+                translation : {x: 10, y: 10, z: 30},
+                rotation : {x: 0, y: -10, z: 0},
+                reverseAnimation : {duration : 200, easing : 'easeOutQuad'}
+            },
+            lines : {
+                translation : {x: 10, y: 10, z: [0,70]},
+                rotation : {x: 0, y: 0, z: -2},
+                reverseAnimation : {duration : 2000, easing : 'easeOutExpo'}
+            },
+            caption : {
+                rotation : {x: 0, y: 0, z: 2},
+                reverseAnimation : {duration : 200, easing : 'easeOutQuad'}
+            },
+            overlay : {
+                translation : {x: 10, y: -10, z: 0},
+                rotation : {x: 0, y: 0, z: 2},
+                reverseAnimation : {duration : 2000, easing : 'easeOutExpo'}
+            },
+            shine : {
+                translation : {x: 100, y: 100, z: 0},
+                reverseAnimation : {duration : 200, easing : 'easeOutQuad'}
+            }
+        }
+    },
+    {
+        movement: {
+            imgWrapper : {
+                rotation : {x: -5, y: 10, z: 0},
+                reverseAnimation : {duration : 900, easing : 'easeOutCubic'}
+            },
+            caption : {
+                translation : {x: 30, y: 30, z: [0,40]},
+                rotation : {x: [0,15], y: 0, z: 0},
+                reverseAnimation : {duration : 1200, easing : 'easeOutExpo'}
+            },
+            overlay : {
+                translation : {x: 10, y: 10, z: [0,20]},
+                reverseAnimation : {duration : 1000, easing : 'easeOutExpo'}
+            },
+            shine : {
+                translation : {x: 100, y: 100, z: 0},
+                reverseAnimation : {duration : 900, easing : 'easeOutCubic'}
+            }
+        }
+    },
+    {
+        movement: {
+            imgWrapper : {
+                rotation : {x: -5, y: 10, z: 0},
+                reverseAnimation : {duration : 50, easing : 'easeOutQuad'}
+            },
+            caption : {
+                translation : {x: 20, y: 20, z: 0},
+                reverseAnimation : {duration : 200, easing : 'easeOutQuad'}
+            },
+            overlay : {
+                translation : {x: 5, y: -5, z: 0},
+                rotation : {x: 0, y: 0, z: 6},
+                reverseAnimation : {duration : 1000, easing : 'easeOutQuad'}
+            },
+            shine : {
+                translation : {x: 50, y: 50, z: 0},
+                reverseAnimation : {duration : 50, easing : 'easeOutQuad'}
+            }
+        }
+    },
+    {
+        movement: {
+            imgWrapper : {
+                translation : {x: 0, y: -8, z: 0},
+                rotation : {x: 3, y: 3, z: 0},
+                reverseAnimation : {duration : 1200, easing : 'easeOutExpo'}
+            },
+            lines : {
+                translation : {x: 15, y: 15, z: [0,15]},
+                reverseAnimation : {duration : 1200, easing : 'easeOutExpo'}
+            },
+            overlay : {
+                translation : {x: 0, y: 8, z: 0},
+                reverseAnimation : {duration : 600, easing : 'easeOutExpo'}
+            },
+            caption : {
+                translation : {x: 10, y: -15, z: 0},
+                reverseAnimation : {duration : 900, easing : 'easeOutExpo'}
+            },
+            shine : {
+                translation : {x: 50, y: 50, z: 0},
+                reverseAnimation : {duration : 1200, easing : 'easeOutExpo'}
+            }
+        }
+    },
+    {
+        movement: {
+            lines : {
+                translation : {x: -5, y: 5, z: 0},
+                reverseAnimation : {duration : 1000, easing : 'easeOutExpo'}
+            },
+            caption : {
+                translation : {x: 15, y: 15, z: 0},
+                rotation : {x: 0, y: 0, z: 3},
+                reverseAnimation : {duration : 1500, easing : 'easeOutElastic', elasticity : 700}
+            },
+            overlay : {
+                translation : {x: 15, y: -15, z: 0},
+                reverseAnimation : {duration : 500,easing : 'easeOutExpo'}
+            },
+            shine : {
+                translation : {x: 50, y: 50, z: 0},
+                reverseAnimation : {duration : 500, easing : 'easeOutExpo'}
+            }
+        }
+    },
+    {
+        movement: {
+            imgWrapper : {
+                translation : {x: 5, y: 5, z: 0},
+                reverseAnimation : {duration : 800, easing : 'easeOutQuart'}
+            },
+            caption : {
+                translation : {x: 10, y: 10, z: [0,50]},
+                reverseAnimation : {duration : 1000, easing : 'easeOutQuart'}
+            },
+            shine : {
+                translation : {x: 50, y: 50, z: 0},
+                reverseAnimation : {duration : 800, easing : 'easeOutQuart'}
+            }
+        }
+    },
+    {
+        movement: {
+            lines : {
+                translation : {x: 40, y: 40, z: 0},
+                reverseAnimation : {duration : 1500, easing : 'easeOutElastic'}
+            },
+            caption : {
+                translation : {x: 20, y: 20, z: 0},
+                rotation : {x: 0, y: 0, z: -5},
+                reverseAnimation : {duration : 1000, easing : 'easeOutExpo'}
+            },
+            overlay : {
+                translation : {x: -30, y: -30, z: 0},
+                rotation : {x: 0, y: 0, z: 3},
+                reverseAnimation : {duration : 750, easing : 'easeOutExpo'}
+            },
+            shine : {
+                translation : {x: 100, y: 100, z: 0},
+                reverseAnimation : {duration : 750, easing : 'easeOutExpo'}
+            }
+        }
+    }];
+
+    function init() {
+        var idx = 0;
+        [].slice.call(document.querySelectorAll('a.tilter')).forEach(function(el, pos) { 
+            idx = pos%2 === 0 ? idx+1 : idx;
+            new TiltFx(el, tiltSettings[idx-1]);
+        });
+    }
+
+    // Preload all images.
+    imagesLoaded(document.querySelector('main'), function() {
+        document.body.classList.remove('loading');
+        init();
+    });
+
+    // REMOVE THIS!
+    // For Demo purposes only. Prevent the click event.
+    [].slice.call(document.querySelectorAll('a[href="#"]')).forEach(function(el) {
+        el.addEventListener('click', function(ev) { ev.preventDefault(); });
+    });
+
+    var pater = document.querySelector('.pater'),
+        paterSVG = pater.querySelector('.pater__svg'),
+        pathEl = paterSVG.querySelector('path'),
+        paths = {default: pathEl.getAttribute('d'), active: paterSVG.getAttribute('data-path-hover')};
+
+    pater.addEventListener('mouseenter', function() {
+        anime.remove(pathEl);
+        anime({
+            targets: pathEl,
+            d: paths.active,
+            duration: 400,
+            easing: 'easeOutQuad'
+        });
+    });
+
+    pater.addEventListener('mouseleave', function() {
+        anime.remove(pathEl);
+        anime({
+            targets: pathEl,
+            d: paths.default,
+            duration: 400,
+            easing: 'easeOutExpo'
+        });
+    });
+})();
